7316d904dd23a301795070c544d6f682cd8a89d1
COUNTRY\country <hnaras@naver.com>
2017-05-16 01:16:48 +0900

    Chapter 4 Example code

---
+++ src/main/java/jpabook/model/entity/Item.java
@@ -0,0 +1,26 @@
+package jpabook.model.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * Created by country on 2017-05-16.
+ */
+@Entity
+@Getter
+@Setter
+public class Item {
+
+    @Id @GeneratedValue
+    @Column(name = "ITEM_ID")
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+}
---
+++ src/main/java/jpabook/model/entity/OrderStatus.java
@@ -0,0 +1,8 @@
+package jpabook.model.entity;
+
+/**
+ * Created by country on 2017-05-16.
+ */
+public enum OrderStatus {
+    ORDER, CANCEL
+}
